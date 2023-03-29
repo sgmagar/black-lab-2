45a77684a214bdbe7e5f3df69fb03a930f9e422f
Saurav <sp.gharti@gmail.com>
2023-03-29 13:53:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-lab-2",
+  url: "https://black-lab-2.botics.co",
+  api: "https://black-lab-2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
